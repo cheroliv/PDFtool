1afba686bd092174dc40c7166e13297047c806d1
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-12-27 23:51:39 +0100

    hide action bar

--- app/src/main/java/com/cheroliv/pdf/MainActivity.kt
+++ app/src/main/java/com/cheroliv/pdf/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.cheroliv.pdf
 
-import android.content.res.Configuration.*
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.graphics.Color.DKGRAY
-import android.graphics.Color.MAGENTA
 import android.graphics.Typeface.MONOSPACE
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getColor
+import com.cheroliv.pdf.R.color.purple_200
 import com.cheroliv.pdf.StyleDsl.applyColor
 import com.cheroliv.pdf.StyleDsl.applyFont
 import com.cheroliv.pdf.StyleDsl.changeFontSize
@@ -15,19 +17,20 @@ import com.cheroliv.pdf.databinding.ActivityMainBinding.inflate
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         inflate(layoutInflater).apply {
             setContentView(root)
             styleWith(selectFileLabelId) {
                 applyFont { MONOSPACE }
                 changeFontSize { 18 }
                 applyColor {
-                    when (root
-                        .context
+                    when (context
                         .resources
                         .configuration
                         .uiMode
                         .and(UI_MODE_NIGHT_MASK)) {
-                        UI_MODE_NIGHT_YES -> MAGENTA
+                        UI_MODE_NIGHT_YES ->
+                            getColor(context, purple_200)
                         else -> DKGRAY
                     }
                 }
