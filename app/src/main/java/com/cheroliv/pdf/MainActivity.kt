bb3146a598f5f1db9b114ee55de820642a98926e
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-12-30 01:54:28 +0100

    kiss

--- app/src/main/java/com/cheroliv/pdf/MainActivity.kt
+++ app/src/main/java/com/cheroliv/pdf/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.cheroliv.pdf
 
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.Color.DKGRAY
+import android.graphics.Typeface.MONOSPACE
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getColor
+import com.cheroliv.pdf.R.color.purple_200
+import com.cheroliv.pdf.StyleDsl.applyColor
+import com.cheroliv.pdf.StyleDsl.applyFont
+import com.cheroliv.pdf.StyleDsl.changeFontSize
+import com.cheroliv.pdf.StyleDsl.styleWith
+import com.cheroliv.pdf.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         launchHomeScreen(layoutInflater)
+    }
+
+    private fun launchHomeScreen(
+        layoutInflater: LayoutInflater
+    ) = apply {
+        supportActionBar?.hide()
+        inflate(layoutInflater).apply {
+            setContentView(root)
+            designInstructionMessage(selectFileLabelId)
+        }
+    }
+
+    private fun designInstructionMessage(textView: TextView) {
+        styleWith(textView) {
+            applyFont { MONOSPACE }
+            changeFontSize { 18 }
+            applyColor {
+                when (UI_MODE_NIGHT_YES) {
+                    context
+                        .resources
+                        .configuration
+                        .uiMode
+                        .and(UI_MODE_NIGHT_MASK) -> getColor(
+                        context,
+                        purple_200
+                    )
+                    else -> DKGRAY
+                }
+            }
+        }
     }
 }
